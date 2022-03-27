9dc57d97859d256947aacd120f1558035269f671
Nary Hang <naryhang@gmail.com>
2022-03-27 14:17:16 -0700

    Add files via upload

---
+++ ProcessingClasses.py
@@ -0,0 +1,96 @@
+#------------------------------------------#
+# Title: Processing Classes
+# Desc: A Module for processing Classes
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+# DBiesinger, 2030-Jan-02, Extended functionality to add tracks
+# Nhang, 2022-Mar-26, Work on TODOs - add code required
+#------------------------------------------#
+
+if __name__ == '__main__':
+    raise Exception('This file is not meant to ran by itself')
+
+import DataClasses as DC
+
+class DataProcessor:
+    """Processing the data in the application"""
+    @staticmethod
+    def add_CD(CDInfo, table):
+        """function to add CD info in CDinfo to the inventory table.
+
+
+        Args:
+            CDInfo (tuple): Holds information (ID, CD Title, CD Artist) to be added to inventory.
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime.
+
+        Returns:
+            None.
+
+        """
+        
+        cdId, title, artist = CDInfo
+        try:
+            cdId = int(cdId)
+        except:
+            raise Exception('ID must be an Integer!')
+        row = DC.CD(cdId, title, artist)
+        table.append(row)
+
+    @staticmethod
+    def select_cd(table: list, cd_idx: int) -> DC.CD:
+        """selects a CD object out of table that has the ID cd_idx
+
+        Args:
+            table (list): Inventory list of CD objects.
+            cd_idx (int): id of CD object to return
+
+        Raises:
+            Exception: If id is not in list.
+
+        Returns:
+            row (DC.CD): CD object that matches cd_idx
+
+        """
+        # TO DONE add code as required
+        try:
+            cd_idx = int(cd_idx)
+        except ValueError as e:
+                print("ID should be an integer")
+                print(e.__doc__)
+        for row in table:
+                if row.cd_id == cd_idx:
+                    return row
+        raise Exception('This cd/album index does not exist')
+        
+        
+
+
+    @staticmethod
+    def add_track(track_info: tuple, cd: DC.CD) -> None:
+        """adds a Track object with attributes in track_info to cd
+
+
+        Args:
+            track_info (tuple): Tuple containing track info (position, title, Length).
+            cd (DC.CD): cd object the tarck gets added to.
+
+        Raises:
+            Exception: DESCraised in case position is not an integer.
+
+        Returns:
+            None: DESCRIPTION.
+
+        """
+
+        # TO DONE add code as required
+        trkPosition, trkTitle, trkLength = track_info
+        try:
+            trkPosition = int(trkPosition)
+        except:
+            raise Exception('Position must be an Integer')
+        track = DC.Track(trkPosition, trkTitle, trkLength)
+        cd.add_track(track)
+        
+        
+
+
